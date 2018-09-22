fab24ff9218458d6cec913c0f034ed4d124c25e1
Julius Bacosa <juliusbacosa@gmail.com>
2018-09-22 18:14:30 +0800

    test

---
+++ Palindrome/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Palindrome
+{
+    public static bool IsPalindrome(string word)
+    {
+        var ary = word.ToCharArray();
+        Array.Reverse(ary);
+        return word.ToLower()
+            .Equals((new string(ary).ToLower()));
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine(Palindrome.IsPalindrome("Deleveled"));
+    }
+}
--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace TwoSum
+class TwoSum
 {
-    class Program
+    public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
     {
-        static void Main(string[] args)
+        throw new NotImplementedException("Waiting to be implemented.");
+    }
+
+    public static void Main(string[] args)
+    {
+        Tuple<int, int> indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+        if (indices != null)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(indices.Item1 + " " + indices.Item2);
         }
     }
 }
