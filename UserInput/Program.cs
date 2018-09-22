d0815eac943ce4c2b12d639d8d1787e87ff3cd31
Julius Bacosa <juliusbacosa@gmail.com>
2018-09-22 18:05:48 +0800

    test

---
+++ UserInput/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+public class TextInput
+{
+    protected StringBuilder _value;
+    public TextInput()
+    {
+        _value = new StringBuilder();
+    }
+    public virtual void Add(char c)
+    {
+        _value.Append(c);
+    }
+    public string GetValue()
+    {
+        return _value.ToString();
+    }
+}
+
+public class NumericInput : TextInput
+{
+    public override void Add(char c)
+    {
+        if (char.IsDigit(c))
+        {
+            _value.Append(c);
+        }
+    }
+}
+
+public class UserInput
+{
+    public static void Main(string[] args)
+    {
+        TextInput input = new NumericInput();
+        input.Add('1');
+        input.Add('a');
+        input.Add('0');
+        Console.WriteLine(input.GetValue());
+        Console.ReadLine();
+    }
+}
