3b88e4e7c2aeb6f41da30c27ec20e91f229ac908
Julius Bacosa <juliusbacosa@gmail.com>
2018-09-22 11:31:45 +0800

    Add project files.

---
+++ TESTDOMEEXAMSCSHARP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TESTDOMEEXAMSCSHARP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
