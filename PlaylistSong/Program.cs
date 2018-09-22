3d1dc511d06099d4c2607d9f1b32a524c8a64e77
Julius Bacosa <juliusbacosa@gmail.com>
2018-09-22 17:44:22 +0800

    testdome

---
+++ PlaylistSong/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Song
+{
+    private string name;
+    public Song NextSong { get; set; }
+
+    public Song(string name)
+    {
+        this.name = name;
+    }
+
+    public bool IsRepeatingPlaylist()
+    {
+        return Pop(this);
+    }
+
+    private int index = 0;
+    Dictionary<int,string> playList = new Dictionary<int, string>();
+    public bool Pop(Song song)
+    {
+        if (playList.ContainsValue(song.name))
+        {
+            return true;
+        }else if (song.NextSong == null)
+        {
+            return false;
+        }
+        else
+        {
+            playList.Add(index, song.name);
+            ++index;
+            return Pop(song.NextSong);
+        }
+    }
+    
+    public static void Main(string[] args)
+    {
+        Song first = new Song("Hello");
+        Song second = new Song("Eye of the tiger");
+      
+        first.NextSong = second;
+        second.NextSong = first;
+
+        Console.WriteLine(first.IsRepeatingPlaylist());
+        
+    }
+}
--- TESTDOMEEXAMSCSHARP/Program.cs
+++ TESTDOMEEXAMSCSHARP/Program.cs
@@ -2,11 +2,40 @@
 
 namespace TESTDOMEEXAMSCSHARP
 {
+    public class Node
+    {
+        public int Value { get; set; }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+
+        public Node(int value, Node left, Node right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+
     class Program
     {
+        public static bool Contains(Node root, int value)
+        {
+           return root.Value == value ? 
+                      true : 
+                      (root.Value < value) ?
+                        ((root.Right != null) && Contains(root.Right, value)) :
+                            ((root.Left != null) && Contains(root.Left, value));
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Node n1 = new Node(1, null, null);
+            Node n3 = new Node(3, null, null);
+            Node n2 = new Node(2, n1, n3);
+            Console.WriteLine(Contains(n2, 1));
+            Console.ReadLine();
         }
     }
 }
