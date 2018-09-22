a8ccb3a7769b26b9ef48a593e227d96e5b43e83c
Julius Bacosa <juliusbacosa@gmail.com>
2018-09-22 19:13:16 +0800

    sum

--- TwoSum/Program.cs
+++ TwoSum/Program.cs
@@ -5,15 +5,39 @@ class TwoSum
 {
     public static Tuple<int, int> FindTwoSum(IList<int> list, int sum)
     {
-        throw new NotImplementedException("Waiting to be implemented.");
+        
+        var i = 0;
+        foreach (var item in list)
+        {
+            var j = 0;
+            foreach (var item2 in list)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                if ((item + item2) == sum)
+                {
+                    return Tuple.Create(i, j);
+                }
+
+                ++j;
+            }
+            ++i;
+        }
+
+        return null;
     }
 
     public static void Main(string[] args)
     {
         Tuple<int, int> indices = FindTwoSum(new List<int>() { 3, 1, 5, 7, 5, 9 }, 10);
+
         if (indices != null)
         {
             Console.WriteLine(indices.Item1 + " " + indices.Item2);
         }
+
     }
 }
