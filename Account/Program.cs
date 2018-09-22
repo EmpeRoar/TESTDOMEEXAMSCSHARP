3f56faf9f0a34c10f3fc0a08cf265360dc0afd82
Julius Bacosa <juliusbacosa@gmail.com>
2018-09-22 19:48:16 +0800

    test

--- Account/Program.cs
+++ Account/Program.cs
@@ -2,6 +2,8 @@
 
 public class Account
 {
+
+
     [Flags]
     public enum Access
     {
@@ -12,8 +14,36 @@ public class Account
         Modify
     }
 
+   
+    public enum Writer
+    {
+        Submit = Access.Submit,
+        Modify = Access.Modify
+    }
+
+  
+    public enum Editor
+    {
+        Publish = Access.Publish,
+        Delete = Access.Delete,
+        Comment = Access.Comment
+    }
+
+    [Flags]
+    public enum Owner
+    {
+        Submit = Access.Submit,
+        Modify = Access.Modify,
+        Publish = Access.Publish,
+        Delete = Access.Delete,
+        Comment = Access.Comment
+    }
+
+
+
+
     public static void Main(string[] args)
     {
-        //Console.WriteLine(Access.Writer.HasFlag(Access.Delete)); //Should print: "False"
+        Console.WriteLine(Access.Writer.HasFlag(Access.Delete)); //Should print: "False"
     }
 }
