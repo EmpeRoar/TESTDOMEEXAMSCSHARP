fee6dfd35a18712fb3f2d84f5c4b4ef93ce53a08
Julius Bacosa <juliusbacosa@gmail.com>
2018-09-22 20:11:25 +0800

    sorted search

---
+++ DecoratorStream/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecoratorStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SortedSearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SortedSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
