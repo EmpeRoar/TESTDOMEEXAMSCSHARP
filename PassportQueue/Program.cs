e232c9ea5e22b6ebac6450272e28da498e29266a
Julius Bacosa <juliusbacosa@gmail.com>
2018-09-23 17:35:30 +0800

    test

---
+++ PassportQueue/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+public class Entry
+{
+    private static Queue _queue = new Queue();
+    public void Enter(string passportNumber)
+    {
+        _queue.Enqueue(passportNumber);
+    }
+
+    public string Leave()
+    {
+        return _queue.Count != 0 ? _queue.Dequeue().ToString() : null;
+    }
+
+    public static void Main(string[] args)
+    {
+        Entry entry = new Entry();
+        entry.Enter("AB54321");
+        entry.Enter("UK32032");
+        Console.WriteLine(entry.Leave());
+        Console.WriteLine(entry.Leave());
+        Console.ReadLine();
+    }
+}
